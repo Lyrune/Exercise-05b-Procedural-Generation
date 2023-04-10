3ca24de55d85801f9cd5b1288fd6f96743eb8a03
Lyrune <122567031+Lyrune@users.noreply.github.com>
2023-04-09 22:57:50 -0400

    finishes the assignment
    we're movinnn

--- Global.gd
+++ Global.gd
@@ -2,4 +2,11 @@ extends Node
 
 func _unhandled_input(event):
 	if event.is_action_pressed("menu"):
-		get_tree().quit()
+		var menu = get_node_or_null("/root/Game/Menu")
+		if menu != null:
+			if not menu.visible:
+				menu.show()
+				get_tree().paused = true
+			else:
+				menu.hide()
+				get_tree().paused = false
